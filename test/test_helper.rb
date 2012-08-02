e614a482bb3402147155b27a009c8ed9941d258b
radiospiel <eno@open-lab.org>
2012-08-02 16:00:01 +0200

    Simplified test helper; properly sets test environment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,7 @@
 require 'rubygems'
-require 'bundler'
-begin
-  Bundler.setup(:default, :development, :test)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
+require 'bundler/setup'
+
+ENV["RACK_ENV"] ||= "test"
 
 require 'ruby-debug'
 require 'simplecov'
