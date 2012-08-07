78bc916496141955ebaa75adfc490da9f1001665
radiospiel <eno@open-lab.org>
2012-08-07 21:44:06 +0200

    Improves deployment code.

--- lib/bountybase/tasks/deployment.rb
+++ lib/bountybase/tasks/deployment.rb
@@ -3,20 +3,62 @@ module Deployment
   
   attr :target, true
 
-  def apps
-    `cat .git/config  | grep heroku.*#{target} | sed s-.*heroku.com:-- | sed s-.git--`.split(/\n/)
+  def applications
+    @applications ||= sys("cat .git/config  | grep heroku.*#{target} | sed s-.*heroku.com:-- | sed s-.git--").split("\n")
+  end
+
+  # returns "staging-deployment", for example.
+  def instance
+    applications.first.gsub(/-.*/, "-deployment")
   end
   
   def run
     # deploy in $deployment_target
     die "Usage: rake staging deploy" unless Deployment.target
-    die "No configured applications to deploy in #{Deployment.target.inspect}" if apps.empty?
+    die "No configured applications to deploy in #{Deployment.target.inspect}" if applications.empty?
 
-    STDERR.puts "Deploy applications #{apps.inspect}"
+    Bountybase.with_settings "INSTANCE" => instance do
+      Bountybase.with_environment self.target do
+        Bountybase.setup
+        prepare_deployment
+
+        applications.each do |application|
+          prepare_application(application)
+          deploy_application(application)
+        end
+      end
+    end
+  end
+  
+  def prepare_deployment
+    logger.warn "Preparing deployment"
+  end
+    
+  def prepare_application(application)
+    remote_instance = sys "heroku config:get INSTANCE --app #{application}"
+    unless remote_instance == application
+      logger.warn "Setting INSTANCE config at remote to #{application}"
+      sys "heroku config:set INSTANCE=#{application} --app #{application}"
+    end
+  end
+
+  def deploy_application(application)
+    sys "git push #{application} master"
+    logger.warn "Deployed", application
+  end
+
+  def sys(cmd)
+    logger.info "Running: #{cmd}"
+    stdout = Kernel.send "`", "bash -c '#{cmd}'"
+    if $?.exitstatus != 0
+      die "Command failed", cmd
+    end
+    
+    stdout.chomp
   end
   
   def die(*args)
-    STDERR.puts args.join(" ")
+    logger.error *args unless args.empty?
     exit 1
   end
 end
