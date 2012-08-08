f710df259febc8e2c13cd8e5de1e8a873f4fb042
radiospiel <eno@open-lab.org>
2012-08-08 12:47:42 +0200

    Improves deployment script

--- lib/bountybase/tasks/deployment.rb
+++ lib/bountybase/tasks/deployment.rb
@@ -7,9 +7,21 @@ module Deployment
     @applications ||= sys("cat .git/config  | grep heroku.*#{target} | sed s-.*heroku.com:-- | sed s-.git--").split("\n")
   end
 
-  # returns "staging-deployment", for example.
+  # The target role, for example "twirl"
+  def target_role
+    return unless target = applications.first
+    target.split("-").last.gsub(/\d+$/, "")
+  end
+  
+  # The target environment, for example "staging"
+  def target_environment
+    return unless application = applications.first
+    application.split("-").first
+  end
+  
+  # returns "deployment-staging", for example.
   def instance
-    applications.first.gsub(/-.*/, "-deployment")
+    "deployment-#{target}"
   end
   
   def run
@@ -18,44 +30,72 @@ module Deployment
     die "No configured applications to deploy in #{Deployment.target.inspect}" if applications.empty?
 
     Bountybase.with_settings "INSTANCE" => instance do
-      Bountybase.with_environment self.target do
-        Bountybase.setup
-        prepare_deployment
+      Bountybase.setup
+      
+      Event.severity = :debug
 
-        applications.each do |application|
-          prepare_application(application)
-          deploy_application(application)
-        end
+      prepare_deployment
+
+      applications.each do |application|
+        prepare_application(application)
+        deploy_application(application)
       end
     end
   end
   
+  def head(subdir)
+    Dir.chdir(subdir) do
+      sys "git rev-parse --verify --short HEAD"
+    end
+  rescue Errno::ENOENT
+  end
+  
+  def verify_bountybase_versions
+    return unless bountybased = head("vendor/bountybased")
+    return unless bountybased != head("vendor/bountybase")
+
+    die <<-MSG
+bountybased and bountybase versions differ. Please push the bountybase project and update the bountybase submodule:
+
+    pushd vendor/bountybased
+    # commit outstanding changes
+    git push
+    popd
+    pushd vendor/bountybase
+    git pull
+    popd
+    git commit -m "Updated bountybase submodule" vendor/bountybase .gitmodules
+MSG
+  end
+  
   def prepare_deployment
-    logger.warn "Preparing deployment"
+    verify_bountybase_versions
   end
     
   def prepare_application(application)
-    remote_instance = sys "heroku config:get INSTANCE --app #{application}"
+    remote_instance = sys! "heroku config:get INSTANCE --app #{application}"
     unless remote_instance == application
       logger.warn "Setting INSTANCE config at remote to #{application}"
-      sys "heroku config:set INSTANCE=#{application} --app #{application}"
+      sys! "heroku config:set INSTANCE=#{application} --app #{application}"
     end
   end
 
   def deploy_application(application)
-    sys "git push #{application} master"
+    sys! "git push #{application} master"
     logger.warn "Deployed", application
   end
 
   def sys(cmd)
-    logger.info "Running: #{cmd}"
+    logger.debug cmd
     stdout = Kernel.send "`", "bash -c '#{cmd}'"
-    if $?.exitstatus != 0
-      die "Command failed", cmd
-    end
     
-    stdout.chomp
+    stdout.chomp if $?.exitstatus == 0
   end
+
+  def sys!(cmd)
+    sys(cmd) || die("Command failed", cmd)
+  end
+
   
   def die(*args)
     logger.error *args unless args.empty?
