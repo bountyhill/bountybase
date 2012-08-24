012e81cf19d737a8715538e4633b5175e8e5b75f
radiospiel <eno@open-lab.org>
2012-08-24 13:21:50 +0200

    Updates documentation for Bountybase::Attributes

--- lib/bountybase/attributes.rb
+++ lib/bountybase/attributes.rb
@@ -1,67 +1,67 @@
+# The Bountybase::Attributes module defines a number of attributes that
+# influence the behaviour of a running applications within a multi-process 
+# application. These attributes include
+#
+# - environment: e.g. "development", "staging" etc.
+# - role: e.g. "mailer", "web", "twirl" etc.
+# - instance: e.g. "mailer1"
+# - root: the root dir.
+#
+# Note that multiple processes might share the same role attribute but
+# should have a different instance attribute.
+#
+# The configuration is read at runtime from these environment variables:
+#
+# - INSTANCE: is able to set environment, role, *and* instance settings 
+#   in one go. Should be set to "<environment>-<role><instance>", where
+#   both "environment" and "role" consists of letters only and the 
+#   "instance" part consists of digits only.
+# - RACK_ENV, RAILS_ENV: the environment setting
+# - RACK_ROOT, RAILS_ROOT: the application's root directory.
+#
+# The Bountybase::Attributes module is extended into the Bountybase module: 
+# the root, environment, role, and instance settings can be accessed as 
+# Bountybase.root, Bountybase.environment, etc.
+#
 module Bountybase::Attributes
   
   Attributes = ::Bountybase::Attributes
   
-  # The Bountybase::Attributes module defines a number of attributes that shape the behaviour
-  # of a running bountybase instance. These include:
-  # - root: the application's root directory, as read from RAILS_ROOT or RACK_ROOT.
-  # - environment: the application's environment settings; e.g. "development", "staging", etc.
-  #   This is read from the RAILS_ENV, RACK_ENV, or INSTANCE environment settings.
-  # - role: the process' role (within a multiprocess application.) This could be, for example,
-  #   "web" or "mailer". It is read from the INSTANCE environment settings.  
-  # - instance: the process instance within a multiprocess application. This usually consists
-  #   of both role and number, e.g. "web1" or "mailer1".
-  #
-  # To define the different settings one sets the INSTANCE environment variable, in the form of
-  # "<environment>-<role><instance>", where both "environment" and "role" consists only of letters
-  # and the optional "instance" part consists of digits only.
-  #
-  # The Bountybase::Attributes module is extended into the Bountybase module: the root, environment, 
-  # role, and instance settings can therefore be accessed via Bountybase.root etc.
-  #
-
   class Missing < RuntimeError; end
 
-  def self.read(*environment_variables)
-    ENV.values_at(*environment_variables).compact.first
-  end
-
-  
-  def self.read!(*environment_variables)
-    read(*environment_variables) || 
-      raise(Missing, "Missing #{environment_variables.join(", or ")} environment variable.")
-  end
-  
-  # find application root.
+  # Return the application's root directory, as read from RAILS_ROOT or RACK_ROOT.
   def root
-    @root ||= Attributes.read! "RAILS_ROOT", "RACK_ROOT"
+    @root ||= I.from_environment! "RAILS_ROOT", "RACK_ROOT"
   end
 
-  # returns the name of the current environment. 
+  # Return the name of the current environment. Typical return values include
+  # "development", "staging", etc. This setting is read from the RAILS_ENV, 
+  # RACK_ENV, or INSTANCE environment variable, and defaults to "development".
   def environment
-    @environment ||= Attributes.read("RAILS_ENV", "RACK_ENV") ||    # try to read from process environment
-      Attributes.parse_instance.first ||                            # try to read from instance setting
-      "development"                                                 # default to development
+    @environment ||= I.from_environment("RAILS_ENV", "RACK_ENV") ||
+      I.parse_instance.first ||
+      "development"
   end
 
-  # returns the role of the running instance. The role describes the general workload
-  # of the current process. There can be multiple processes with the same role:
-  #
-  # If there are two running twirl instances, they would both share the "twirl" role.
-  # They would not share the instance, which would probably be "twirl1" or  "twirl2".
+  # Returns the process' role within a multiprocess application. The role 
+  # describes the workload an application instance will process; for example, 
+  # "web" or "mailer". 
   def role
     @role ||= begin
-      _, role, _ = *Attributes.parse_instance
+      _, role, _ = *I.parse_instance
       role || raise(Missing, "Cannot determine role.")
     end
   end
 
-  # returns the instance name of the running instance. This name usually contains 
-  # both the role to desccribe the type of instance, and a number to discriminate
-  # amongst components running the same role.
+  # returns the instance name of the running instance. This name usually 
+  # contains both the role to desccribe the type of instance, and a number
+  # to discriminate amongst components running the same role.
+  #
+  # The instance defaults to "test" in the test environment, as there is 
+  # usually no instance setting.
   def instance
     @instance ||= begin
-      _, _, instance = *Attributes.parse_instance
+      _, _, instance = *I.parse_instance
       instance || if environment == "test"
         "test"
       else
@@ -70,26 +70,18 @@ module Bountybase::Attributes
     end
   end
 
-  # parses the name of the INSTANCE environment variable. 
-  def self.parse_instance
-    return [] unless instance = Attributes.read("INSTANCE")
-    
-    unless instance.to_s =~ /^([a-zA-Z]+)-([a-zA-Z]+)(\d*)$/
-      raise ArgumentError, "Invalid INSTANCE environment setting: #{instance.inspect} "
-    end
-    
-    [ $1, $2, "#{$2}#{$3}" ]
-  rescue Missing
-  end
-  
   # -- modify settings ---------------------------------------------------------------
   
+  # Modify RACK_ENV and RAILS_ENV setting for the duration of a block; 
+  # this is needed to test this module.
   def with_environment(environment, &block) # :nodoc:
     with_settings "RACK_ENV" => environment, "RAILS_ENV" => environment, &block
   end
 
-  def with_settings(settings, &block)
-    reset_attributes!
+  # Modify environment variables for the duration of a block; 
+  # this is needed to test this module.
+  def with_settings(settings, &block) #:nodoc:
+    @root = @environment = @role = @instance = nil
 
     old_env = {}
     settings.each { |key, value| 
@@ -100,18 +92,33 @@ module Bountybase::Attributes
     yield
   ensure
     ENV.update old_env
-    reset_attributes!
-  end
-
-  private
-  
-  # resets the name of the environment setting for the duration of a block. 
-  # This method should not be used outside of tests.
-  def reset_attributes!
     @root = @environment = @role = @instance = nil
   end
+
+  # Private methods used by Bountybase::Attributes
+  module I #:nodoc:all
+    def self.from_environment(*environment_variables)
+      ENV.values_at(*environment_variables).compact.first
+    end
+
+    def self.from_environment!(*environment_variables)
+      from_environment(*environment_variables) || 
+        raise(Bountybase::Attributes::Missing, "Missing #{environment_variables.join(", or ")} environment variable.")
+    end
+
+    # parses the INSTANCE environment variable. 
+    def self.parse_instance
+      return [] unless instance = from_environment("INSTANCE")
+
+      unless instance.to_s =~ /^([a-zA-Z]+)-([a-zA-Z]+)(\d*)$/
+        raise ArgumentError, "Invalid INSTANCE environment setting: #{instance.inspect} "
+      end
+
+      [ $1, $2, "#{$2}#{$3}" ]
+    rescue Bountybase::Attributes::Missing
+    end
+  end
+  
 end
 
-module Bountybase
-  extend Attributes
-end
+Bountybase.extend Bountybase::Attributes
