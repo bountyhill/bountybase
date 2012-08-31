efe9ae89539bb4b4c22e5a608e9f01008a9474b1
radiospiel <eno@open-lab.org>
2012-08-31 11:59:58 +0200

    Moves Bountybase::VERSION into a separate file.
    This allows to load the bountybase version string without
    loading the entire bountybase code.

--- lib/bountybase.rb
+++ lib/bountybase.rb
@@ -3,24 +3,53 @@ require_relative "kernel/enumerable_ext"
 require_relative "kernel/hash_ext"
 require_relative "kernel/standard_error_ext"
 
-# The Bountybase namespace organizes data access patterns for the Bountyhill application.
-module Bountybase; end
+# The Bountybase namespace organizes code for the Bountyhill applications.
+# A rough breakdown includes the following sub modules:
+#
+# = Configuration
+#
+# - Bountyhill::Attributes: attributes for a running Bountyhill application.
+# - Bountyhill::Config: global configuration for Bountyhill applications.
+# - Bountyhill::Setup: initialize a Bountyhill applications
+#
+# = Bountyhill infrastructure
+#
+# - Bountyhill::Graph: building and querying the Bountyhill graph
+# - Bountyhill::Message: messaging and queueing tasks between different Bountyhill components.
+# - Bountyhill::Metrics: end point for collecting Bountyhill statistics and metrics.
+#
+# = low level code
+#
+# You probably don't need this code.
+#
+# - Bountyhill::HTTP: HTTP and HTTPS requests
+# - Bountyhill::Neo4j: Neo4j interface
+# - Bountyhill::TwitterAPI: Twitter API request methods.
+#
+# = Loading the Bountybase code base.
+#
+# The Bountybase code is not distributed as a gem, because it contains 
+# secret credentials in the config.yml file. We chose this way because
+# there is a heroku limitation in referring private code from an application.
 
-require_relative "bountybase/config"
-
+# Bountybase is expected to be installed in its target apps as a Git submodule
+# under vendor/bountybase. To load and set up the bountybase code base you use
+#
+#   require_relative "vendor/bountybase/setup"
+#
+# For your local development environment you may link a local bountybase 
+# repository into vendor/bountybased; this allows you to work in the
+# bountybase codebase and the target app at the same time, without having
+# to push changes to bountybase inbetween.
 module Bountybase
+  #
+  # All instance methods in the Bountybase module will be added onto the
+  # Bountybase object 
   extend self
-
-  VERSION = "0.1"
-  
-  # -- register a tweet
-
-  # return true if this is a bountytweet.
-  def bountytweet?(tweet)
-    true
-  end
 end
 
+require_relative "bountybase/version"
+require_relative "bountybase/config"
 require_relative "bountybase/attributes"
 require_relative "bountybase/http"
 require_relative "bountybase/event"
