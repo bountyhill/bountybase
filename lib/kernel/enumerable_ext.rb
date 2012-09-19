087d5fc59fa9380b471cae9ac2addb7d0a125606
radiospiel <eno@open-lab.org>
2012-09-19 22:25:20 +0200

    Adds Enumerable#by(symbol) to deal with Hash entries.

--- lib/kernel/enumerable_ext.rb
+++ lib/kernel/enumerable_ext.rb
@@ -6,14 +6,28 @@ module Enumerable
   # this method does not return a Hash of Arrays, but a Hash of
   # Objects instead.
   #
-  #   a = [ 1, 2, 3]
+  #   a = [ 1, 2, 3 ]
   #   h = a.by { |int| int*int }
   #   # => { 1 => 1, 2 => 4, 3 => 9} 
   #
-  def by(&proc)
+  #   a = [ { key: 1 }, { key: 2 }, { key: 3} ]
+  #   h = a.by(:key)
+  #   # => { 1 => { key: 1 }, 2 => { key: 2 }, 3 => { key: 3} ]
+  #
+  def by(key = nil, &proc)
+    if key && block_given?
+      raise ArgumentError, "You cannot pass both a key and a callback to Enumerable#by"
+    end
+
     r = {}
-    each do |row|
-      r[yield(row)] = row
+    if key
+      each do |row|
+        r[row[key]] = row
+      end
+    else
+      each do |row|
+        r[yield(row)] = row
+      end
     end
     r
   end
